2ee1ff183e3e17f83dc43962c0d8e9da63e5fde4
Kasprzyk <kasprzyk@protonmail.com>
2023-02-08 19:34:22 +0100

    Settings screen

--- Assets/UIDocumentController.cs
+++ Assets/UIDocumentController.cs
@@ -3,13 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+
 public class UIDocumentController : MonoBehaviour
 {
+    public static readonly string MainMenuAssetName = "MainMenu";
+    public static readonly string SettingsScreenAssetName = "SettingsScreen";
+
+    string currentlySelectedScreen = MainMenuAssetName;
+
     void OnEnable()
     {
         UIDocument uiDocument = GetComponent<UIDocument>();
         Button exitButton = uiDocument.rootVisualElement.Q("ExitButton") as Button;
         exitButton.RegisterCallback<ClickEvent>(ExitApplication);
+        Button settingsButton = uiDocument.rootVisualElement.Q("SettingsButton") as Button;
+        settingsButton.RegisterCallback<ClickEvent>(OpenSettings);
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown("escape")) {
+            if (currentlySelectedScreen == SettingsScreenAssetName) {
+                OpenScreen(MainMenuAssetName);
+                OnEnable();
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+    void OpenScreen (string assetName) {
+        currentlySelectedScreen = assetName;
+        GameObject.FindFirstObjectByType<UnityEngine.UIElements.UIDocument>().visualTreeAsset = Resources.Load<VisualTreeAsset>(assetName);
+    }
+
+    void OpenSettings(ClickEvent clickEvent)
+    {
+        OpenScreen(SettingsScreenAssetName);
     }
 
     void ExitApplication(ClickEvent clickEvent)
