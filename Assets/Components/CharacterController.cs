0e3077acb1a3cd795847c2dba81aec9a9c50ff46
Kasprzyk <kasprzyk@protonmail.com>
2023-02-08 21:20:34 +0100

    Spawned enemies

--- Assets/Components/CharacterController.cs
+++ Assets/Components/CharacterController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class CharacterController : MonoBehaviour
 {
     [SerializeField] float speed = 0.2f;
 
---
+++ Assets/Components/EnemiesSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesSpawner : MonoBehaviour
+{
+    [SerializeField] int numberOfEnemies = 1000;
+
+    void Start()
+    {
+        float maximumVertialPositionOfEnemy = Camera.main.transform.position.z * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        float maximumHorizontalPositionOfEnemy = maximumVertialPositionOfEnemy * Camera.main.aspect;
+        for (int index = 0; index < numberOfEnemies; index++) {
+            GameObject enemy = Instantiate<GameObject>(Resources.Load<GameObject>("Enemy"), transform);
+
+            enemy.transform.localPosition = new Vector2(maximumHorizontalPositionOfEnemy * 2 * (UnityEngine.Random.value - 0.5f), maximumVertialPositionOfEnemy * 2 * (UnityEngine.Random.value - 0.5f));
+        }
+    }
+}
