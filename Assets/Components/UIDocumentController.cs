76f78e55bad93cfdfa34bc8030fce3ccf6e29694
Kasprzyk <kasprzyk@protonmail.com>
2023-02-09 01:23:16 +0100

    Back button on the settings screen

--- Assets/Components/UIDocumentController.cs
+++ Assets/Components/UIDocumentController.cs
@@ -7,28 +7,23 @@ using UnityEngine.SceneManagement;
 
 public class UIDocumentController : MonoBehaviour
 {
-    public static readonly string MainMenuAssetName = "MainMenu";
-    public static readonly string SettingsScreenAssetName = "SettingsScreen";
+    private static readonly string MainMenuAssetName = "MainMenu";
+    private static readonly string SettingsScreenAssetName = "SettingsScreen";
 
-    string currentlySelectedScreen = MainMenuAssetName;
+    private UIDocument uiDocument;
+    private string currentlySelectedScreen;
 
     void OnEnable()
     {
-        UIDocument uiDocument = GetComponent<UIDocument>();
-        Button playButton = uiDocument.rootVisualElement.Q("PlayButton") as Button;
-        playButton.RegisterCallback<ClickEvent>(Play);
-        Button exitButton = uiDocument.rootVisualElement.Q("ExitButton") as Button;
-        exitButton.RegisterCallback<ClickEvent>(ExitApplication);
-        Button settingsButton = uiDocument.rootVisualElement.Q("SettingsButton") as Button;
-        settingsButton.RegisterCallback<ClickEvent>(OpenSettings);
+        uiDocument = GetComponent<UIDocument>();
+        OpenMainMenu(null);
     }
 
-    void Update ()
+    void Update()
     {
         if (Input.GetKeyDown("escape")) {
             if (currentlySelectedScreen == SettingsScreenAssetName) {
-                OpenScreen(MainMenuAssetName);
-                OnEnable();
+                OpenMainMenu(null);
             }
             else if (currentlySelectedScreen == MainMenuAssetName)
             {
@@ -37,22 +32,34 @@ public class UIDocumentController : MonoBehaviour
         }
     }
 
-    void OpenScreen (string assetName) {
+    void OpenScreen(string assetName) {
         currentlySelectedScreen = assetName;
-        GameObject.FindFirstObjectByType<UnityEngine.UIElements.UIDocument>().visualTreeAsset = Resources.Load<VisualTreeAsset>(assetName);
+        uiDocument.visualTreeAsset = Resources.Load<VisualTreeAsset>(assetName);
     }
 
-    void Play (ClickEvent clickEvent)
+    void Play(ClickEvent clickEvent)
     {
         SceneManager.LoadScene("1");
     }
 
-    void OpenSettings (ClickEvent clickEvent)
-    {
-        OpenScreen(SettingsScreenAssetName);
+    void OpenMainMenu(ClickEvent clickEvent) {
+        OpenScreen(MainMenuAssetName);
+        Button playButton = uiDocument.rootVisualElement.Q("PlayButton") as Button;
+        playButton.RegisterCallback<ClickEvent>(Play);
+        Button exitButton = uiDocument.rootVisualElement.Q("ExitButton") as Button;
+        exitButton.RegisterCallback<ClickEvent>(ExitApplication);
+        Button settingsButton = uiDocument.rootVisualElement.Q("SettingsButton") as Button;
+        settingsButton.RegisterCallback<ClickEvent>(OpenSettings);
     }
 
-    void ExitApplication (ClickEvent clickEvent)
+    void OpenSettings(ClickEvent clickEvent)
+    {
+        OpenScreen(SettingsScreenAssetName);
+        Button settingsButton = uiDocument.rootVisualElement.Q("BackButton") as Button;
+        settingsButton.RegisterCallback<ClickEvent>(OpenMainMenu);
+    }
+
+    void ExitApplication(ClickEvent clickEvent)
     {
         Application.Quit();
     }
